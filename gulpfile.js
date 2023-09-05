a73f652ce57f26f9bb9542397b89433ee2a38d68
edriso <edriso.dev@gmail.com>
2023-09-06 00:37:30 +0300

    add tailwindcss, and complete folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,47 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('autoprefixer');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const replace = require('gulp-replace');
 const sass = require('gulp-sass')(require('sass'));
+const ejs = require('gulp-ejs');
+const postcss = require('gulp-postcss');
+const postcssImport = require('postcss-import');
+const tailwindcss = require('tailwindcss');
+const cssnano = require('cssnano');
 
 // File path variables
 const files = {
-  scssPath: 'app/scss/**/*.scss',
-  jsPath: 'app/js/**/*.js',
-  htmlPath: 'views/**/*.html',
+  scssPath: 'src/scss/**/*.scss',
+  jsPath: 'src/js/**/*.js',
+  ejsPath: 'src/views/**/*.ejs',
+  htmlPath: 'dist/views/**/*.html',
 };
+
+// ejs task
+function ejsTask() {
+  return gulp
+    .src('src/views/pages/*.ejs')
+    .pipe(ejs())
+    .pipe(rename({ extname: '.html' }))
+    .pipe(gulp.dest('dist/views'));
+}
 
 // SCSS task
 function scssTask() {
+  const plugins = [
+    postcssImport(),
+    tailwindcss(),
+    autoprefixer(),
+    cssnano()
+  ];
+
   return gulp
     .src(files.scssPath)
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(autoprefixer({ overrideBrowserslist: ['last 2 versions'] }))
-    .pipe(rename('style.min.css'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(rename('style.css'))
     .pipe(gulp.dest('dist'));
 }
 
@@ -27,7 +49,7 @@ function scssTask() {
 function jsTask() {
   return gulp
     .src(files.jsPath)
-    .pipe(concat('all.js'))
+    .pipe(concat('script.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
 }
@@ -38,20 +60,26 @@ function cacheBustTask() {
   return gulp
     .src([files.htmlPath])
     .pipe(replace(/cb=\d+/g, `cb=${cbNumber}`))
-    .pipe(gulp.dest('views'));
+    .pipe(gulp.dest('dist/views'));
 }
 
 // Watch task
 function watchTask() {
-  gulp.watch([files.scssPath, files.jsPath], gulp.parallel(scssTask, jsTask));
+  gulp.watch(
+    [files.scssPath, files.jsPath, files.ejsPath],
+    gulp.parallel(scssTask, jsTask, ejsTask),
+  );
 }
 
 // Build task
-exports.build = gulp.series(gulp.parallel(scssTask, jsTask), cacheBustTask);
+exports.build = gulp.series(
+  gulp.parallel(scssTask, jsTask, ejsTask),
+  cacheBustTask,
+);
 
 // Default task
 exports.default = gulp.series(
-  gulp.parallel(scssTask, jsTask),
+  gulp.parallel(scssTask, jsTask, ejsTask),
   cacheBustTask,
-  watchTask
+  watchTask,
 );
