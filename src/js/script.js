662d11346181723a31efdea803b0c77e7ae5cea6
edriso <edriso.dev@gmail.com>
2023-09-07 12:03:36 +0300

    complete layout and navigation

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,44 @@
-const z = 1;
+// Sidebar Toggle
+const sidebar = document.getElementById('logo-sidebar');
+if (sidebar) {
+  const toggleButton = document.querySelector(
+    '[data-drawer-toggle="logo-sidebar"]'
+  );
+  const toggleSidebar = () => {
+    sidebar.classList.toggle('-translate-x-full');
 
-console.log(z);
+    if (sidebar.classList.contains('-translate-x-full')) {
+      // Sidebar is closed
+      document.body.classList.remove('overflow-hidden');
+      backdrop.remove();
+    } else {
+      // Sidebar is open
+      document.body.classList.add('overflow-hidden');
+      document.body.appendChild(backdrop);
+    }
+  };
+  const backdrop = document.createElement('div');
+  backdrop.classList.add(
+    'bg-gray-900',
+    'bg-opacity-50',
+    'dark:bg-opacity-80',
+    'fixed',
+    'inset-0',
+    'z-30'
+  );
+  toggleButton.addEventListener('click', toggleSidebar);
+  backdrop.addEventListener('click', toggleSidebar);
+
+  const userDropdownBtn = document.querySelector(
+    '[data-dropdown-toggle="dropdown-user"]'
+  );
+  const userDropdownMenu = document.querySelector('#dropdown-user');
+  userDropdownBtn.addEventListener('click', () => {
+    userDropdownMenu.classList.toggle('hidden');
+  });
+
+  const logoutAction = document.querySelector('#logout-action');
+  logoutAction.addEventListener('click', (e) => {
+    console.log('logged out');
+  });
+}
